8dc299167e2cbfe5037b4352a664b7cd848919ed
carlosbernard0 <carlosbernardo774@gmail.com>
2024-05-12 15:56:26 -0300

    exercises 29 and 30 solveds

--- vs-code/modulo1/exerciciosCodifica2/ex29.js
+++ vs-code/modulo1/exerciciosCodifica2/ex29.js
@@ -4,3 +4,37 @@
 // c) da diagonal principal;
 // d) todos os elementos da matriz M.
 // Escrever essas somas e a matriz
+
+let M = new Array(5);
+
+for (let i = 0; i < M.length; i++) {
+    M[i] = new Array(5);
+}
+
+let sumOfFourLine = 0;
+let sumOfTwoColumn = 0;
+let sumOfPrincipalAngule = 0;
+let sumAllElements = 0;
+for (let i = 0; i < M.length; i++) {
+    for (let j = 0; j < M[i].length; j++) {
+        let value = Math.floor(Math.random()*5);
+        M[i][j] = value;
+        if(i == 4){
+            sumOfFourLine+=value;
+        }
+        if(j == 2){
+            sumOfTwoColumn+= value;
+        }
+        if(i == j){
+            sumOfPrincipalAngule += value;
+        }
+        sumAllElements+=value;
+    }
+}
+
+console.table(M);
+
+console.log('The sum of 4 line is: '+sumOfFourLine);
+console.log('The sum of 2 column is: '+sumOfTwoColumn);
+console.log('The sum of principal angule is: '+sumOfPrincipalAngule);
+console.log('The sum of all elements is: '+sumAllElements);
--- vs-code/modulo1/exerciciosCodifica2/ex30.js
+++ vs-code/modulo1/exerciciosCodifica2/ex30.js
@@ -1,3 +1,36 @@
 // Escrever um algoritmo que lê uma matriz M(5,5) e cria 2 vetores SL(5) e SC(5) que
 // contenham, respectivamente, as somas das linhas e das colunas de M. Escrever a matriz
 // e os vetores criados.
+let M = new Array(5);
+
+for (let i = 0; i < M.length; i++) {
+    M[i] = new Array(5);
+}
+
+let SL = new Array(5);
+let SC = new Array(5);
+
+for (let i = 0; i < M.length; i++) {
+    for (let j = 0; j < M[i].length; j++) {
+        let value = Math.floor(Math.random()*3);
+        M[i][j] = value;
+    }
+}
+
+for (let i = 0; i < M.length; i++) {
+    let sumAllLine = 0
+    let sumAllColumn = 0
+    for (let j = 0; j < M[i].length; j++) {
+        sumAllLine+= M[i][j];
+        sumAllColumn+= M[j][i];
+    }
+    SL[i] = sumAllLine;   
+    SC[i] = sumAllColumn;
+}
+
+console.table('Matriz:');
+console.table(M);
+console.table('Soma das Linhas:');
+console.table(SL);
+console.table('Soma das Colunas:');
+console.table(SC);
